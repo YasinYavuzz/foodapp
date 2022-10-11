654c2ee7339149818da935c284e979fb0adb420f
yasinyavuz <yavuzyasin.eem@gmail.com>
2022-10-11 15:49:03 +0300

    assets folder created in files added, AppNotificationIcon widget completed

--- lib/widgets/categorieswidget/appbar/appbar.dart
+++ lib/widgets/categorieswidget/appbar/appbar.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:foodapp/widgets/categorieswidget/appbar/appbaricon/appbarchevrondownicon.dart';
 import 'package:foodapp/widgets/categorieswidget/appbar/appbaricon/appbarlocationicon.dart';
+import 'package:foodapp/widgets/categorieswidget/appbar/appbaricon/appbarnotificationicon.dart';
 import 'package:foodapp/widgets/categorieswidget/appbar/appbartext/appbarboldtext.dart';
 import 'package:foodapp/widgets/categorieswidget/appbar/appbartext/appbarlighttext.dart';
 
@@ -14,42 +15,57 @@ class appBar extends StatefulWidget {
 }
 
 class _appBarState extends State<appBar> {
+  int notificationNumber = 7;
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(top: 50),
       width: double.infinity,
-      height: 250,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      height: 55,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            margin: EdgeInsets.only(top: 50, right: 260),
-            width: 83,
-            height: 20,
-            child: AppBarBoldText(),
-          ),
-          Row(
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
-                margin: EdgeInsets.only(left: 15),
-                width: 30,
-                height: 30,
-                child: AppBarLocationIcon(),
+                margin: EdgeInsets.only(right: 107),
+                //margin: EdgeInsets.only(top: 50, right: 260),
+                width: 83,
+                height: 20,
+                child: AppBarBoldText(),
               ),
-              Container(
-                margin: EdgeInsets.only(left: 0,top: 8),
-                width: 160,
-                height: 25,
-                child: AppBarLightText(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 1),
-                width: 28,
-                height: 25,
-                child: AppBarChevronDownIcon(),
-              ),
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 10),
+                    //margin: EdgeInsets.only(left: 15),
+                    width: 30,
+                    height: 30,
+                    child: AppBarLocationIcon(),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 9),
+                    //margin: EdgeInsets.only(left: 0, top: 8),
+                    width: 160,
+                    height: 25,
+                    child: AppBarLightText(),
+                  ),
+                  Container(
+                    //margin: EdgeInsets.only(top: 1),
+                    width: 28,
+                    height: 25,
+                    child: AppBarChevronDownIcon(),
+                  ),
+                ],
+              )
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 0,right: 25),
+            child: AppBarNotificationIcon(notificationNumber: notificationNumber),
           )
+          
         ],
       ),
     );
